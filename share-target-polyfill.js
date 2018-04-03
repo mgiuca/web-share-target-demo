7c28ffd6e1abab6d03307d6f3e881b77c068f8c4
Eric Willigers <ericwilligers@chromium.org>
2018-04-03 16:49:10 +1000

    Web Share Target using webmanifest params

--- share-target-polyfill.js
+++ share-target-polyfill.js
@@ -3,29 +3,42 @@
 (() => {
   function receiveMessage(event) {
     const manifestURL = document.querySelector('link[rel=manifest]').href;
-    console.log(manifestURL);
     fetch(manifestURL)
     .then((response) => {
       return response.json();
     })
     .then((myJson) => {
-      if (myJson.share_target && myJson.share_target.url_template) {
+      if (myJson.share_target && myJson.share_target.action && myJson.share_target.params) {
+        const action = myJson.share_target.action;
+        const params = myJson.share_target.params;
+        const pairs = [];
+        for (let key of ['title', 'text', 'url']) {
+          if (params[key] && event.data[key]) {
+            pairs.push(encodeURIComponent(params[key]) + '=' + encodeURIComponent(event.data[key]));
+          }
+        }
+        const url = action + '?' + pairs.join('&');
+        console.log('URL = "' + url + '"');
+        document.getElementById('target').contentWindow.location.href = url;
+      } else if (myJson.share_target && myJson.share_target.url_template) {
+        // e.g. "url_template": "share-target-destination.html?received_title={title}&received_text={text}&received_url={url}"
         let url = myJson.share_target.url_template;
-        let index = url.length;
-        while (index >= 0) {
-          index = url.lastIndexOf('{', index);
-          if (index < 0) {
+        let length = url.length;
+        for (;;) {
+          const index = url.lastIndexOf('{', length);
+          const endIndex = url.lastIndexOf('}', length);
+          if (index < 0 && endIndex < 0) {
             console.log('URL = "' + url + '"');
             document.getElementById('target').contentWindow.location.href = url;
             return;
           }
-          const endIndex = url.indexOf('}', index);
-          if (endIndex < 0) {
-            return;
+          if (index < 0 || endIndex <= index) {
+            return; // Unmatched { or }.
           }
           const key = url.substring(index + 1, endIndex);
-          const data = event.data[key] || '';
+          const data = (new Set(['title', 'text', 'url']).has(key) && event.data[key]) || '';
           url = url.substring(0, index) + encodeURIComponent(data) + url.substring(endIndex + 1);
+          length = index;
         }
       }
     });
