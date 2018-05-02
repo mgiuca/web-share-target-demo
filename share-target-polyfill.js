60e155be786241bf7985ea66d7b3d66b3d7c7836
Eric Willigers <ericwilligers@chromium.org>
2018-05-02 17:26:08 +1000

    POST

--- share-target-polyfill.js
+++ share-target-polyfill.js
@@ -3,23 +3,46 @@
 (() => {
   function receiveMessage(event) {
     const manifestURL = document.querySelector('link[rel=manifest]').href;
+    console.log('Fetching manifest ' + manifestURL);
     fetch(manifestURL)
     .then((response) => {
+      console.log('Fetched manifest ' + manifestURL);
       return response.json();
     })
     .then((myJson) => {
       if (myJson.share_target && myJson.share_target.action && myJson.share_target.params) {
         const action = myJson.share_target.action;
+        const method = (myJson.share_target.method || '').toUpperCase();
         const params = myJson.share_target.params;
-        const pairs = [];
-        for (let key of ['title', 'text', 'url']) {
-          if (params[key] && event.data[key]) {
-            pairs.push(encodeURIComponent(params[key]) + '=' + encodeURIComponent(event.data[key]));
+        if (method === 'POST') {
+          const form = document.createElement('form');
+          form.action = action;
+          form.method = 'POST';
+          for (let key of ['title', 'text', 'url']) {
+            if (params[key] && event.data[key]) {
+              const input = document.createElement('input');
+              input.type = 'hidden';
+              input.id = params[key];
+              input.name = params[key];
+              input.value = event.data[key];
+              form.appendChild(input);
+            }
           }
+          const targetBody = document.getElementById('target').contentWindow.document.body;
+          targetBody.appendChild(form);
+          form.submit();
+          targetBody.removeChild(form);
+        } else {
+          const pairs = [];
+          for (let key of ['title', 'text', 'url']) {
+            if (params[key] && event.data[key]) {
+              pairs.push(encodeURIComponent(params[key]) + '=' + encodeURIComponent(event.data[key]));
+            }
+          }
+          const url = action + '?' + pairs.join('&');
+          console.log('URL = "' + url + '"');
+          document.getElementById('target').contentWindow.location.href = url;
         }
-        const url = action + '?' + pairs.join('&');
-        console.log('URL = "' + url + '"');
-        document.getElementById('target').contentWindow.location.href = url;
       } else if (myJson.share_target && myJson.share_target.url_template) {
         // e.g. "url_template": "share-target-destination.html?received_title={title}&received_text={text}&received_url={url}"
         let url = myJson.share_target.url_template;
