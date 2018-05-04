dd4ffad6f741bee225d3985f8903981e3808e31b
Eric Willigers <ericwilligers@chromium.org>
2018-05-04 15:16:18 +1000

    Service Worker

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
 'use strict';
 
 self.addEventListener('fetch', function(event) {
+  console.log('Received fetch event: ' + event.request.method + ' ' + event.request.url);
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
         // Cache hit - return response
         if (response) {
+          console.log('Cache hit');
           return response;
         }
+        console.log('Cache miss');
         return fetch(event.request);
       }
     )
