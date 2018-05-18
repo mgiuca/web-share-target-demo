5b0cf00db9abb9a93c491003a6c64a3b04e52fc7
Eric Willigers <>
2018-05-18 13:25:46 +1000

    passes

--- share-target-destination.py
+++ share-target-destination.py
@@ -18,25 +18,34 @@ class MainPage(RequestHandler):
     def post(self):
         main_template = JINJA_ENVIRONMENT.get_template('share-target-destination.template.html')
 
-        main_template_values = {
-          'generation_location': 'server-side',
-          'received_title': escape(self.request.POST.get('received_title', '')),
-          'received_text': escape(self.request.POST.get('received_text', '')),
-          'received_url': escape(self.request.POST.get('received_url', ''))
-        }
-        print(self.request.headers)
-        print(main_template_values)
+        attachments = self.request.POST.getall('received_file')
+        print('attachments:' + str(attachments))
+        # attachments:[u'']   when nothing is attached
 
-        uploaded_file = self.request.POST.get('received_file')
-        if uploaded_file:
-          print('Name of file uploaded: ' + uploaded_file.filename)
-          print('Type of file uploaded: ' + uploaded_file.type)
-          file_contents = uploaded_file.file.read()
+        # [x for x in range(2, num) if is_prime(x)]
+
+        attachments2 = [{'content': f.file.read(),
+                         'filename': f.filename} for f in attachments if f != '']
+        print('attachments2:' + str(attachments2))
+        print(attachments2[0])
+        print(attachments2[0]['filename'])
+        print(attachments2[0]['content'])
+
+
+        if True:
+          main_template_values = {
+            'generation_location': 'server-side',
+            'received_title': escape(self.request.POST.get('received_title', '')),
+            'received_text': escape(self.request.POST.get('received_text', '')),
+            'received_url': escape(self.request.POST.get('received_url', ''))
+          }
+          print(self.request.headers)
+          print(main_template_values)
+
+          file_contents = attachments2[0]['content']
           main_template_values['received_file'] = file_contents
-        else:
-          print('No file uploaded')
 
-        self.response.write(main_template.render(main_template_values))
+          self.response.write(main_template.render(main_template_values))
 
 
 app = WSGIApplication([
