e2ace6f07c6bd2eec38e3c6f76250ebb3c4c2b1e
Matt Giuca <mgiuca@chromium.org>
2018-05-21 13:28:38 +1000

    Use async/await and arrow function notation.

--- sw.js
+++ sw.js
@@ -4,90 +4,79 @@
 
 let handleClientSide = true;
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-
+self.addEventListener('fetch', event => {
   // Ideally, share-target-destination.template.html would be cached in advance.
   function respondToShare(event) {
-    event.respondWith(
-      fetch('share-target-destination.template.html')
-        .then(function (response) {
-          return response.text()
-            .then(function(page) {
+    event.respondWith((async () => {
+      let response = await fetch('share-target-destination.template.html');
+      let page = await response.text();
+      let formData = await event.request.formData();
+      const title = formData.get('received_title') || '';
+      const text = formData.get('received_text') || '';
+      const url = formData.get('received_url') || '';
 
-              return event.request.formData()
-                .then(function(formData) {
-                  const title = formData.get('received_title') || '';
-                  const text = formData.get('received_text') || '';
-                  const url = formData.get('received_url') || '';
+      const init = {
+        status: 200,
+        statusText: 'OK',
+        headers: {'Content-Type': 'text/html'}
+      };
 
-                  const init = {
-                      status: 200,
-                      statusText: "OK",
-                      headers: {'Content-Type': 'text/html'}
-                  };
+      let body = page.replace('{{generation_location}}', 'client-side')
+                     .replace('{{received_title}}', title)
+                     .replace('{{received_text}}', text)
+                     .replace('{{received_url}}', url);
 
-                  let body = page
-                    .replace('{{generation_location}}', 'client-side')
-                    .replace("{{received_title}}", title)
-                    .replace("{{received_text}}", text)
-                    .replace("{{received_url}}", url);
+      const file_fields = ['received_html_files', 'received_css_files'];
 
-                  const file_fields = [
-                    "received_html_files",
-                    "received_css_files"
-                  ];
+      let field_index = 0;
 
-                  let field_index = 0;
+      let files = undefined;
+      let file_contents = '';
+      let index = 0;
 
-                  let files = undefined;
-                  let file_contents = '';
-                  let index = 0;
+      function prepareField() {
+        files = formData.getAll(
+            file_fields[field_index]);  // sequence of File objects
+        file_contents = '';
+        index = 0;
+      }
 
-                  function prepareField() {
-                    files = formData.getAll(file_fields[field_index]); // sequence of File objects
-                    file_contents = '';
-                    index = 0;
-                  }
+      prepareField();
 
-                  prepareField();
+      return new Promise(resolve => {
+        function progress() {
+          while (index === files.length) {
+            body = body.replace(
+                '{{' + file_fields[field_index] + '}}', file_contents);
 
-                  return new Promise(function(resolve, reject) {
-                    function progress() {
-                      while (index === files.length) {
-                        body = body
-                          .replace('{{' + file_fields[field_index] + '}}', file_contents);
+            ++field_index;
+            if (field_index === file_fields.length) {
+              resolve(new Response(body, init));
+              return;
+            }
+            prepareField();
+          }
 
-                        ++field_index;
-                        if (field_index === file_fields.length) {
-                          resolve(new Response(body, init));
-                          return;
-                        }
-                        prepareField();
-                      }
+          const fileReader = new FileReader();
+          fileReader.onload = fileLoadedEvent => {
+            const textFromFileLoaded = fileLoadedEvent.target.result;
+            if (index > 0) {
+              file_contents += ', ';
+            }
+            file_contents += textFromFileLoaded;
+            index += 1;
+            progress();
+          };
+          fileReader.readAsText(files[index], 'UTF-8');
+        }
 
-                      const fileReader = new FileReader();
-                      fileReader.onload = function(fileLoadedEvent) {
-                        const textFromFileLoaded = fileLoadedEvent.target.result;
-                        if (index > 0) {
-                          file_contents += ', ';
-                        }
-                        file_contents += textFromFileLoaded;
-                        index += 1;
-                        progress();
-                      };
-                      fileReader.readAsText(files[index], "UTF-8");
-                    }
-
-                    progress();
-                  });
-                })
-            })
-        })
-    );
+        progress();
+      });
+    })());
   }
 
   // console.log('Received fetch event: ' + event.request.method + ' ' + event.request.url);
